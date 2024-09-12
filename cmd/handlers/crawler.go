2db57baf48660379be04ecba0c7cd9e20b758b96
xtasysensei <maiwuyashem@gmail.com>
2024-09-12 15:12:58 +0100

    fixed crawler throwing cryptic errors by making visitedURL and lst variables local instead of global

--- cmd/handlers/crawler.go
+++ cmd/handlers/crawler.go
@@ -1,29 +1,53 @@
 package handlers
 
 import (
+	"fmt"
 	"log"
 	"net/url"
 	"strings"
+	"sync"
 
 	"github.com/anaskhan96/soup"
 )
 
-var visited []string
-var lst []string
+func Crawler(baseURL string, depth int, wg *sync.WaitGroup, mu *sync.Mutex) {
+	visitedURL := make(map[string]bool)
+	if depth <= 0 {
+		return
+	}
 
-func Crawler(baseURL string) {
-	urls, err := getLinks(baseURL)
+	urls, err := getLinks(baseURL, visitedURL)
 	if err != nil {
 		log.Fatalf("failed to get urls from %s: %v ", baseURL, err)
 	}
-	for _, singleURL := range urls {
-		go func() {
-			ConnectAndRequest(singleURL)
-		}()
+	for _, lin := range urls {
+		fmt.Println(lin)
+	}
+
+	if len(urls) > 0 {
+		for _, singleURL := range urls {
+
+			mu.Lock()
+			if visitedURL[singleURL] {
+				mu.Unlock()
+				continue // Skip this URL if it has already been visited
+			}
+			visitedURL[singleURL] = true
+			mu.Unlock()
+
+			wg.Add(1)
+			go func(goUrl string) {
+				defer wg.Done()
+				fmt.Printf("[%s]---------------------->[%s]\n", baseURL, goUrl)
+				Crawler(goUrl, depth-1, wg, mu)
+			}(singleURL)
+
+			//ConnectAndRequest(singleURL)
+		}
 	}
 }
-func getLinks(baseURL string) ([]string, error) {
-
+func getLinks(baseURL string, visited map[string]bool) ([]string, error) {
+	var lst []string
 	resp, err := soup.Get(baseURL)
 	if err != nil {
 		log.Fatalf("failed to get %s with soup: %v", baseURL, err)
@@ -41,22 +65,10 @@ func getLinks(baseURL string) ([]string, error) {
 			continue
 		} else if strings.HasPrefix(linkURL, "mailto:") || strings.HasPrefix(linkURL, "javascript:") {
 			continue
-		} else if stringInSlice(urlJoin, visited) {
-			continue
 		} else {
 			lst = append(lst, urlJoin)
-			visited = append(visited, urlJoin)
+			visited[urlJoin] = false
 		}
 	}
 	return lst, nil
-}
-
-// checks if the joined url is in the visited slice
-func stringInSlice(a string, list []string) bool {
-	for _, b := range list {
-		if b == a {
-			return true
-		}
-	}
-	return false
 }
