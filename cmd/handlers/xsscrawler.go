1cdd42815b9bc5da1f8acff3bad57366cd584772
xtasysensei <maiwuyashem@gmail.com>
2024-09-16 13:35:28 +0100

    changed crawler and crawler.go to xsscrawler and xsscrawler.go

---
+++ cmd/handlers/xsscrawler.go
@@ -0,0 +1,76 @@
+package handlers
+
+import (
+	"fmt"
+	"log"
+	"net/url"
+	"strings"
+	"sync"
+
+	"github.com/anaskhan96/soup"
+)
+
+func XSSCrawler(baseURL string, depth int, wg *sync.WaitGroup, mu *sync.Mutex, useragent []string) {
+	visitedURL := make(map[string]bool)
+	if depth <= 0 {
+		return
+	}
+
+	urls, err := getLinks(baseURL, visitedURL)
+	if err != nil {
+		log.Printf("failed to get urls from %s: %v", baseURL, err)
+		return
+	}
+
+	if len(urls) > 0 {
+		for _, singleURL := range urls {
+
+			mu.Lock()
+			if visitedURL[singleURL] {
+				mu.Unlock()
+				continue
+			}
+			visitedURL[singleURL] = true
+			mu.Unlock()
+
+			wg.Add(1)
+			go func(goUrl string) {
+				defer wg.Done()
+				if depth == 0 {
+					XSSCrawler(goUrl, depth, wg, mu, useragent)
+				} else {
+					XSSCrawler(goUrl, depth-1, wg, mu, useragent)
+				}
+				ConnectAndRequest(goUrl)
+			}(singleURL)
+
+		}
+	}
+}
+func getLinks(baseURL string, visited map[string]bool) ([]string, error) {
+	var lst []string
+	resp, err := soup.Get(baseURL)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get %s with soup: %w", baseURL, err)
+	}
+	doc := soup.HTMLParse(resp)
+	links := doc.FindAll("a")
+	for _, link := range links {
+		linkURL := link.Attrs()["href"]
+
+		urlJoin, err := url.JoinPath(baseURL, linkURL)
+		if err != nil {
+			log.Printf("link join failed: %v", err)
+			continue
+		}
+		if strings.HasPrefix(linkURL, "http://") || strings.HasPrefix(linkURL, "https://") {
+			continue
+		} else if strings.HasPrefix(linkURL, "mailto:") || strings.HasPrefix(linkURL, "javascript:") {
+			continue
+		} else {
+			lst = append(lst, urlJoin)
+			visited[urlJoin] = false
+		}
+	}
+	return lst, nil
+}
